e44fb7eb0df61b7bd46ea7e999ba56986ad9bacd
凯 <loyalpartner@163.com>
2019-05-20 13:28:09 +0800

    移除 src/Main.hs。

---
+++ src/JSONClass.hs
@@ -0,0 +1,16 @@
+module JSONClass  where
+
+import SimpleJSON
+
+type JSONError = String
+
+class JSON a where
+  toJValue :: a -> JValue
+  fromJValue :: JValue -> Either JSONError a
+
+
+instance JSON JValue where
+  toJValue = id
+  fromJValue = Right
+  
+  
